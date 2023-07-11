86a6bc5fced48d7c7788db62994c57f2ed84db22
Derek <dbeck1207@gmail.com>
2023-07-11 13:48:34 -0500

    jquery logic for SPA

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("#home").show();
+
+  $("nav a").click(function (e) {
+    e.preventDefault();
+    var page = $(this).attr("href");
+    console.log($("div"));
+    $("div").hide();
+    $(page).show();
+  });
+});
